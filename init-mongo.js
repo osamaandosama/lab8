b6b61febac7364fa2c2944a4f1d23eacc9ca6a9c
osamaandosama <78632887+osamaandosama@users.noreply.github.com>
2026-04-07 13:05:11 +0200

    Update init-mongo.js

--- init-mongo.js
+++ init-mongo.js
@@ -2,5 +2,6 @@ db = db.getSiblingDB('taskdb');
 db.tasks.insertMany([
   { id: 1, name: 'Milk', status: 'done' },
   { id: 2, name: 'Eggs', status: 'done' },
-  { id: 3, name: 'Bread', status: 'pending' }
+  { id: 3, name: 'Bread', status: 'pending' },
+  { id: 7, name: 'Tea', status: 'pending' }
 ]);
